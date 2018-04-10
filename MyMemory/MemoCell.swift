d8bea83d4287617307dab7d5866314a1f9616dc2
장원석 <jang-wonseog@jang-wonseogs-mac.local>
2018-04-10 22:04:56 +0900

    메모 목록, 작성, 상세보기 UI작성 완료

---
+++ MyMemory/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  MyMemory
+//
+//  Created by 장원석 on 2018. 4. 10..
+//  Copyright © 2018년 장원석. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
